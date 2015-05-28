5047e765ed7d90b66b52d5b05f75292c26a61188
Esha Maharishi <eshamaharishi@Chaitanyas-MacBook-Pro-2.local>
2015-05-28 17:11:48 -0500

    generalized script for pushing csv files into postgres

---
+++ pushdata.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e 
+
+DBHOST="dssgsummer2014postgres.c5faqozfo86k.us-west-2.rds.amazonaws.com"
+DB="training_2015" 
+DBUSER=$1
+INPUT_DATA_FILE=$2
+TABLE_NAME=$3
+
+# formulate CREATE TABLE in SQL statement using csv file
+#csvsql $INPUT_DATA_FILE > table.sql
+# manually edit/verify CREATE TABLE statement
+vim table.sql
+
+CREATE_TABLE=$(cat table.sql)
+
+RUN_ON_MYDB="psql -X -h $DBHOST -U $DBUSER -d $DB"
+
+echo $RUN_ON_MYDB
+
+# drop table if already exists
+$RUN_ON_MYDB <<SQL 
+drop table if exists $TABLE_NAME;
+SQL
+
+
+# run create table statement
+$RUN_ON_MYDB -c "$CREATE_TABLE"
+
+cat $INPUT_DATA_FILE | sed 's/\$//g' | sed 's/\ //g' | psql -h "$DBHOST" -U $DBUSER -d $DB -c "\COPY $TABLE_NAME FROM STDIN WITH CSV HEADER;" 
